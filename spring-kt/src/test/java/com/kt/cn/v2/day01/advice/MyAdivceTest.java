067b3d6c2605c649706e5a45dc9e535fa846c6a8
hujie <hujie20081122@163.com>
2022-01-13 18:32:08 +0800

    add interceptor

--- spring-kt/src/test/java/com/kt/cn/v2/day01/advice/MyAdivceTest.java
+++ spring-kt/src/test/java/com/kt/cn/v2/day01/advice/MyAdivceTest.java
@@ -2,6 +2,7 @@ package com.kt.cn.v2.day01.advice;
 
 import com.kt.cn.v2.day01.advisor.Animal;
 import com.kt.cn.v2.day01.advisor.Dog;
+import com.kt.cn.v2.day01.advisor.MyAfterReturningAdvce;
 import com.kt.cn.v2.day01.advisor.MyMethodBeforeAdvice;
 import org.junit.Test;
 import org.springframework.aop.framework.ProxyFactory;
@@ -9,7 +10,7 @@ import org.springframework.aop.framework.ProxyFactory;
 public class MyAdivceTest {
 
     @Test
-    public void test(){
+    public void test1(){
 
         //前置增强
         //1、实例化bean和增强
@@ -24,5 +25,30 @@ public class MyAdivceTest {
         //3、生成代理实例
         Animal proxyDog = (Animal) proxyFactory.getProxy();
         proxyDog.sayHello("二哈",3);
+    }
+
+    @Test
+    public void test2(){
+        //后置增强
+        //1、实例化bean和增强
+        Animal dog =  new Dog();
+        MyAfterReturningAdvce advce = new MyAfterReturningAdvce();
+
+        //2、创建ProxyFactory并设置代理目标和增强
+        ProxyFactory proxyFactory = new ProxyFactory();
+        proxyFactory.setTarget(dog);
+        proxyFactory.addAdvice(advce);
+
+        //3、生成代理
+        Animal proxyDog = (Animal) proxyFactory.getProxy();
+        proxyDog.sayHello("二哈",3);
+    }
+
+    @Test
+    public void test3(){
+
+        //环绕增强
+        // 1、实例化bean和增强
+
     }
 }
