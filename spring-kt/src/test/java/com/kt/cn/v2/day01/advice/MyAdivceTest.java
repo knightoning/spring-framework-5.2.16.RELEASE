43e4cf6f8a9515ac5c08a0166ac67690d7b0feef
hujie <hujie20081122@163.com>
2022-01-13 18:38:19 +0800

    add inter

--- spring-kt/src/test/java/com/kt/cn/v2/day01/advice/MyAdivceTest.java
+++ spring-kt/src/test/java/com/kt/cn/v2/day01/advice/MyAdivceTest.java
@@ -1,9 +1,6 @@
 package com.kt.cn.v2.day01.advice;
 
-import com.kt.cn.v2.day01.advisor.Animal;
-import com.kt.cn.v2.day01.advisor.Dog;
-import com.kt.cn.v2.day01.advisor.MyAfterReturningAdvce;
-import com.kt.cn.v2.day01.advisor.MyMethodBeforeAdvice;
+import com.kt.cn.v2.day01.advisor.*;
 import org.junit.Test;
 import org.springframework.aop.framework.ProxyFactory;
 
@@ -49,6 +46,16 @@ public class MyAdivceTest {
 
         //环绕增强
         // 1、实例化bean和增强
+        Animal dog = new Dog();
+        MyMethodInterceptor advice = new MyMethodInterceptor();
 
+        //2、创建ProxyFactoy并设置代理目标和增强
+        ProxyFactory proxyFactory = new ProxyFactory();
+        proxyFactory.setTarget(dog);
+        proxyFactory.addAdvice(advice);
+
+        //3、生成代理实例
+        Animal proxyDog = (Animal) proxyFactory.getProxy();
+        proxyDog.sayHello("二哈",3);
     }
 }
